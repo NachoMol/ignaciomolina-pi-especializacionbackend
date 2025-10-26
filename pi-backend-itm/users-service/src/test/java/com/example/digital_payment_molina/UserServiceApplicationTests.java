fe4e8b289a5f79baac8b6f11b1860ed39c613ee4
NachoMol <ignaciotmolina@gmail.com>
2025-10-26 20:45:22 -0300

    Implementando Eureka

---
+++ pi-backend-itm/users-service/src/test/java/com/example/digital_payment_molina/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.digital_payment_molina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
